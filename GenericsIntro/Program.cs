6552b4b4facc2de374ce5c7fe9c477a52ffb1ef4
Sami Can Keleş <smcnkls55@gmail.com>
2021-01-25 16:46:29 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin")
+        }
+    }
+}
